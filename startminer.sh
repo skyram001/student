7b8e9964497573b023cba06d64d45ab48c51b9e6
skyram001 <107674140+skyram001@users.noreply.github.com>
2022-07-04 11:06:07 +0700

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMKjgEVM82bB1VCKyFw7PSjxzhViVamfQF.rh1 -p x --cpu 3
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMKjgEVM82bB1VCKyFw7PSjxzhViVamfQF.vps -p x --cpu 3
